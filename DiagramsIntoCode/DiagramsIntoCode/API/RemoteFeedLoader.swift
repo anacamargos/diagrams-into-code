47b47b72563b8e302bbb12fa9e2617edca7a7387
Ana Camargos <alcvlara@sga.pucminas.br>
2020-06-01 09:08:29 -0300

    Creating RemoteFeedLoader

---
+++ DiagramsIntoCode/DiagramsIntoCode/API/RemoteFeedLoader.swift
@@ -0,0 +1,16 @@
+//
+//  RemoteFeedLoader.swift
+//  DiagramsIntoCode
+//
+//  Created by Ana Leticia Camargos on 01/06/20.
+//  Copyright © 2020 Ana Leticia Camargos. All rights reserved.
+//
+
+import Foundation
+
+final class RemoteFeedLoader: FeedLoader {
+    
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        // Do something
+    }
+}
